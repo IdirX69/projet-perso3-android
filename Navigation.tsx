6bf17322d6a6a0b2206207e5a708831a41422bf7
IdirX69 <adil.idirr@gmail.com>
2023-10-29 18:57:02 +0100

    stack navigator without header

--- Navigation.tsx
+++ Navigation.tsx
@@ -7,6 +7,7 @@ import RegisterPage from "./src/pages/RegisterPage";
 import LoginPage from "./src/pages/LoginPage";
 import * as SecureStore from "expo-secure-store";
 import { useUser } from "./src/Context/UserContext";
+import { createStackNavigator } from "@react-navigation/stack";
 
 function BudgetScreen() {
   return <></>;
@@ -15,6 +16,7 @@ function BudgetScreen() {
 function ProfilScreen() {
   return <></>;
 }
+const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
@@ -32,43 +34,54 @@ function Navigation() {
   console.log(dataToken);
 
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            if (route.name == "Home") {
-              iconName = "home";
-            } else if (route.name == "Profile") {
-              iconName = "person-circle";
-            } else if (route.name == "Search") {
-              iconName = "search";
-            } else if (route.name == "Favoris") {
-              iconName = "heart";
-            }
-            if (iconName) {
-              return <Ionicons name={iconName} size={25} color={"black"} />;
-            } else {
-              return null;
-            }
-          },
-        })}
-      >
-        <>
-          <Tab.Screen name="Home" component={BudgetScreen} />
-          <Tab.Screen name="Search" component={ProfilScreen} />
-          <Tab.Screen name="Favoris" component={LoginPage} />
-          <Tab.Screen name="Profile" component={ProfilScreen} />
-        </>
+    <>
+      {dataToken?.token ? (
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Login" component={LoginPage} />
+            <Stack.Screen name="Register" component={RegisterPage} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      ) : (
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              headerShown: false,
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name == "Home") {
+                  iconName = "home";
+                } else if (route.name == "Profile") {
+                  iconName = "person-circle";
+                } else if (route.name == "Search") {
+                  iconName = "search";
+                } else if (route.name == "Favoris") {
+                  iconName = "heart";
+                }
+                if (iconName) {
+                  return <Ionicons name={iconName} size={25} color={"black"} />;
+                } else {
+                  return null;
+                }
+              },
+            })}
+          >
+            <>
+              <Tab.Screen name="Home" component={BudgetScreen} />
+              <Tab.Screen name="Search" component={ProfilScreen} />
+              <Tab.Screen name="Favoris" component={LoginPage} />
+              <Tab.Screen name="Profile" component={ProfilScreen} />
+            </>
 
-        <Tab.Screen
-          name="Register"
-          component={RegisterPage}
-          options={{ tabBarButton: () => null }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+            <Tab.Screen
+              name="Register"
+              component={RegisterPage}
+              options={{ tabBarButton: () => null }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      )}
+    </>
   );
 }
 
