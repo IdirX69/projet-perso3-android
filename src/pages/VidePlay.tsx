1e217add2403fff10d8dfae01547495ec85c2623
IdirX69 <adil.idirr@gmail.com>
2023-12-04 10:43:07 +0100

    video player

--- src/pages/VidePlay.tsx
+++ src/pages/VidePlay.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
-import React from "react";
+import * as React from "react";
+import { View, StyleSheet, Button } from "react-native";
+import { Video, ResizeMode } from "expo-av";
 
-export default function VidePlay() {
+export default function App() {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
-    <View>
-      <Text>VidePlay</Text>
+    <View style={styles.container}>
+      <Video
+        ref={video}
+        style={styles.video}
+        source={{
+          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        useNativeControls
+        resizeMode={ResizeMode.CONTAIN}
+        isLooping
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ecf0f1",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
