53e943900b4856a526a0ad9909f3cee5da57b198
IdirX69 <adil.idirr@gmail.com>
2023-12-11 15:12:50 +0100

    comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -22,23 +22,39 @@ const Comment = () => {
 
   return (
     <View style={styles.wrapper}>
-      {/* Utilisez require pour charger l'image localement */}
       {/* <Image source={require(`${backendUrl}/api/avatars/${user.avatar}`)} /> */}
 
-      {/* Utilisez value et onChangeText pour gérer le texte de l'entrée */}
-      <TextInput
-        style={styles.input}
-        value={comment}
-        onChangeText={(text) => setComment(text)}
-      />
-      <Image style={styles.img} source={require("../../assets/img/send.png")} />
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-around",
+          margin: 5,
+        }}
+      >
+        <TextInput
+          placeholder="Ajouter un commentaire..."
+          style={styles.input}
+          value={comment}
+          onChangeText={(text) => setComment(text)}
+        />
+        <Image
+          style={styles.img}
+          source={require("../../assets/img/send.png")}
+        />
+      </View>
     </View>
   );
 };
 
 export default Comment;
 const styles = StyleSheet.create({
-  wrapper: { backgroundColor: "white", minHeight: "20%", borderRadius: 10 },
-  input: { backgroundColor: "red" },
-  img: { backgroundColor: "green", height: 20, width: 20 },
+  wrapper: {
+    borderColor: "blue",
+    backgroundColor: "white",
+    minHeight: "20%",
+    borderRadius: 10,
+  },
+  input: { backgroundColor: "red", width: "85%" },
+  img: { backgroundColor: "green", height: 26, width: 20, alignSelf: "center" },
 });
